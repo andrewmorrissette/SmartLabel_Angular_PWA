a8900e72933eb31bd6b32d4af7b84e71bf5f8679
andrewmorrissette <66964576+andrewmorrissette@users.noreply.github.com>
2021-05-11 19:19:51 -0400

    Update wordpress.service.ts

--- src/app/services/localWordpress/wordpress.service.ts
+++ src/app/services/localWordpress/wordpress.service.ts
@@ -25,7 +25,7 @@ export class LocalWordpressService {
   //////////////////
   //////CHANGE//////
   //////////////////
-  private personalWordpressSite:string = "https://culturalnexus.msu.edu/wordpress"
+  private personalWordpressSite:string = ""
   //"testingsmartlabel.art.blog/"; //"http://localhost/cultureconnect/"
   private authToken:string = "";
 
@@ -35,7 +35,7 @@ export class LocalWordpressService {
   //DO NOT CHANGE///
   /////////////////
 
-  private wordpressAPI:string="https://culturalnexus.msu.edu/wordpress/wp-json/wp/v2/"; //FINAL URL Determined in constructor
+  private wordpressAPI:string=""; //FINAL URL Determined in constructor
 
   private MasterLevelTag="";  
 
